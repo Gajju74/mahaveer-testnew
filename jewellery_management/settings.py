84204138a84ddff29ba03928dad28c92270a7fe3
Gajju74 <gbanjara852@gmail.com>
2024-10-01 16:54:18 +0530

    add s3 bucket code

--- jewellery_management/settings.py
+++ jewellery_management/settings.py
@@ -166,9 +166,23 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+# AWS S3 settings
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_FILE_OVERWRITE = False
+AWS_LOCATION = 'static'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
 STATIC_URL = '/static/'
+
+# Static and media storage settings
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG: 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
